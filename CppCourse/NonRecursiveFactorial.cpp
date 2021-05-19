ec51cb27c22f4dfef75e885695cb3086c8a43756
divya jharia <divyajhariadj@gmail.com>
2021-05-19 09:42:51 +0530

    [PROGRESS] Factorial Program, Overloading Functions

---
+++ CppCourse/NonRecursiveFactorial.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int fact{ 0 };
+//	int factorial{1};
+//	cout << "Enter any number :" << endl;
+//	cin >> fact;
+//
+//	for (int i = fact; i > 1; i=i-2)
+//	{
+//		factorial = factorial * i * (i-1);
+//
+//	}
+//	cout << factorial;
+//	return 0;
+//}
